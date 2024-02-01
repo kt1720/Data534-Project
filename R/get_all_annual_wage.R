739bfad864c0fc163963120fde65efb992f1d9ec
Isawsomethingb4 <yujinxin01@gmail.com>
2024-02-01 12:33:57 -0800

    structure

--- R/get_all_annual_wage.R
+++
@@ -1,9 +0,0 @@
-get_df_annual_wage <- function(df_split){
-  df <- df_split%>%
-    rowwise()%>%
-    mutate(across(contains('wage_salaire'), ~ifelse(annual_wage_flag_salaire_annuel==0, .*2080, .)))%>%
-    select(-annual_wage_flag_salaire_annuel)%>%
-    arrange(reference_period)
-  return(df)
-}
-df <- get_df_annual_wage(df_split)
