0f87cef2061ee22b9ef24e2a7fb927605a1ac894
Kyle Deng <kyledeng97@gmail.com>
2024-02-02 01:04:10 -0800

    Finish up the map function and make minor adjustment to list_occupations and salary_range

--- R/list_wage_occupations.R
+++
@@ -1,3 +0,0 @@
-list_wage_occupations <- function(dataset, str){
-  return(grep(str, unique(dataset$occupation), ignore.case = T, value = T))
-}
--- R/salary_range.R
+++ R/salary_range.R
@@ -1,4 +1,5 @@
 salary_range <- function(dataset, job, prov){
+  prov <- translate_province(prov)
   dataset %>%
     filter(occupation == job & province == prov) %>%
     summarize(minimum_salary = mean(low_wage), maximum_salary = mean(high_wage))
