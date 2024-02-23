c6c3adc980512e053576b96ae64e8dd6948090c2
Kyle Deng <kyledeng97@gmail.com>
2024-02-23 13:05:21 -0800

    Update pipe operator

--- R/wage_trend.R
+++ R/wage_trend.R
@@ -30,19 +30,19 @@ trend<-function(records,type,filte=100000000,provs=c("Ontario","British Columbia
   positions<-tolower(positions)
   #get overview of all the dataset
   if (type=="overall"){
-    boxplot<-records %>%
-      filter(median_wage<filte) %>%
-      group_by(year,province) %>%
+    boxplot<-records |>
+      filter(median_wage<filte) |>
+      group_by(year,province) |>
       ggplot(varwidth=TRUE) +
       #see the distribution around the nation
       geom_boxplot(aes(x = year, y = median_wage,col=province)) #varwidth = TRUE
 
 
-    lineplot<-records%>%
-      filter(median_wage<filte) %>%
-      group_by(year,province) %>%
+    lineplot<-records |>
+      filter(median_wage<filte) |>
+      group_by(year,province) |>
       #calculate the mean value to draw the trend plot
-      summarize(mean_average=mean(median_wage,na.rm=TRUE),count=n(),.groups="drop") %>%
+      summarize(mean_average=mean(median_wage,na.rm=TRUE),count=n(),.groups="drop") |>
       ggplot() +
       #set group=1 to connect every point with line
       geom_line(aes(x=year,y=mean_average,col=province))+
@@ -55,13 +55,13 @@ trend<-function(records,type,filte=100000000,provs=c("Ontario","British Columbia
   #customerized plot
   else if (type=="specific"){
 
-    line<-records%>%
+    line<-records |>
       #choose the province and type of jobs selected
-      mutate(occupation= tolower(occupation)) %>%
-      filter(province %in% provs ,occupation %in% positions, median_wage<filte) %>%
-      group_by(year,province,occupation) %>%
+      mutate(occupation= tolower(occupation)) |>
+      filter(province %in% provs ,occupation %in% positions, median_wage<filte) |>
+      group_by(year,province,occupation) |>
       #calculate the mean value to draw the trend plot
-      summarize(mean_average=mean(median_wage,na.rm=TRUE),count=n(),.groups="drop") %>%
+      summarize(mean_average=mean(median_wage,na.rm=TRUE),count=n(),.groups="drop") |>
       ggplot() +
       #use col to distinguish type of job and facet to province
       geom_line(aes(x=year,y=mean_average,col=occupation))+
